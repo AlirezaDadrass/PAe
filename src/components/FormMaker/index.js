21d13c5d31f355c308c5241599f8603cf8b85d7b
Alireza Dadrass <alireza.dadrass@hotmail.com>
2023-04-08 00:20:37 +0330

    Component Builder added

--- src/components/FormMaker/index.js
+++ src/components/FormMaker/index.js
@@ -1,3 +1,22 @@
-import Grid2 from '@mui/material/Unstable_Grid2';
-
-
+import * as Mui from "@mui/material";
+import React, { useEffect } from "react";
+export const FormMaker = (props) => {
+  for (var m in props?.model?.methods) {
+    window[m] = props?.model?.methods[m];
+  }
+  const build = function (obj) {
+    if (typeof obj === "string" || obj instanceof String) return obj;
+    try {
+      return obj.children
+        ? Mui[obj.component]
+          ? React.createElement(Mui[obj.component], obj.props, ...obj.children.map((m) => build(m)))
+          : React.createElement(obj.component, obj.props, ...obj.children.map((m) => build(m)))
+        : Mui[obj.component]
+        ? React.createElement(Mui[obj.component], obj.props, ...obj.children.map((m) => build(m)))
+        : React.createElement(obj.component, obj.props, ...obj.children.map((m) => build(m)));
+    } catch (e) {
+      return <></>;
+    }
+  };
+  return <>{props?.model?.buildTree.map((m) => build(m))}</>;
+};
--- src/layouts/javascript-management/index.js
+++ src/layouts/javascript-management/index.js
@@ -3,36 +3,50 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import { useState } from "react";
 import * as monaco from "monaco-editor";
+import { FormMaker } from "components/FormMaker";
 function JavaScriptManagement() {
   const [code, setCode] = useState();
-
   const model = monaco.editor.createModel(
-    `const component = {
+    `return {
       onInitialize: () => { },
       afterRender: () => { },
       onSubmit: () => { },
       afterSubmit: () => { },
-      methodes: {},
+      methods: {
+        test: () => {
+          alert("hahaha")
+        }
+      },
       buildTree: [
         {
-          component: "TextField",
-          propes: {
-            color: "success"
-          }
+          component: "Button",
+          props: {
+            color: "success",
+            onClick: this.test
+          },
+          children: [
+            "hello"
+          ]
         }
       ]
     }`,
     "javascript"
   );
+
   return (
     <DashboardLayout
       style={{ padding: 0, display: "flex", flexDirection: "column", height: "100vh" }}
     >
       <DashboardNavbar />
+      <FormMaker model={code} />
       <JsEditor
         codeModel={model}
         onChange={(c) => {
-          setCode(c);
+          try {
+            let f = new Function(c)();
+            setCode(f);
+          } catch (e) {
+          }
         }}
       />
     </DashboardLayout>
